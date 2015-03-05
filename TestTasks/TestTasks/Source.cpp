392beb6d328deb31c4d06a5baec77453da7bd8c6
Kirill Berezin <enelar@develop-project.ru>
2015-03-05 03:25:28 +0300

    First Task

---
+++ TestTasks/TestTasks/Source.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+auto main()
+{
+  int num;
+  cin >> num;
+
+  auto still_saw_only_zeros = true;
+  const auto bits_in_byte = 8;
+  auto current_bit = sizeof(num) * 8; // start from left
+
+  while (--current_bit >= 0)
+  {
+    auto bit = (num >> current_bit) & 1;
+    if (!bit && still_saw_only_zeros)
+      continue;
+    still_saw_only_zeros = false; // it faster than with compare
+    cout << (char)('0' + bit);
+  }
+  cout << endl;
+}
